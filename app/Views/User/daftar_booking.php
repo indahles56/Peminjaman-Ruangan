eadb4b018b4dc76a0d336fa72185c6234a60a15f
LAPTOP-NAJ6JUDT\WIWIN WINARTI <wiwinwinarti140197@gmail.com>
2023-07-08 10:17:29 +0700

    cek lagi

--- app/Views/User/daftar_booking.php
+++ app/Views/User/daftar_booking.php
@@ -2,47 +2,88 @@
 <html>
 <head>
     <title>Daftar Booking Ruangan</title>
-    <!-- Tambahkan link CSS atau framework CSS lainnya -->
+    <!-- AdminLTE CSS -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/admin-lte/3.1.0/css/adminlte.min.css">
 </head>
-<body>
-    <header>
-        <h1>Daftar Booking Ruangan</h1>
-    </header>
-    <nav>
-        <!-- Tambahkan menu navigasi di sini -->
-    </nav>
-    <main>
-        <h2>Daftar Booking Ruangan</h2>
-        <?php if (!empty($booking)) : ?>
-            <table>
-                <tr>
-                    <th>No</th>
-                    <th>Kode Pinjam</th>
-                    <th>Kode Barang</th>
-                    <th>Tanggal</th>
-                    <th>Waktu Mulai</th>
-                    <th>Waktu Selesai</th>
-                    <th>Kelas</th>
-                    <th>Status</th>
-                    <th>Aksi</th>
-                </tr>
-                <?php foreach ($booking as $row) : ?>
-                    <tr>
-                        <td><?= $row['no'] ?></td>
-                        <td><?= $row['kode_pinjam'] ?></td>
-                        <td><?= $row['kode_ruang'] ?></td>
-                        <td><?= $row['tanggal'] ?></td>
-                        <td><?= $row['waktu_mulai'] ?></td>
-                        <td><?= $row['waktu_selesai'] ?></td>
-                        <td><?= $row['kelas'] ?></td>
-                        <td><?= $row['status'] ?></td>
-                        <td><?= $row['aksi'] ?></td>
-                    </tr>
-                <?php endforeach; ?>
-            </table>
-        <?php else : ?>
-            <p>Tidak ada data booking ruangan.</p>
-        <?php endif; ?>
-    </main>
+<body class="hold-transition sidebar-mini">
+    <div class="wrapper">
+        <!-- Navbar -->
+        <nav class="navbar navbar-expand navbar-white navbar-light">
+            <!-- Left navbar links -->
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">About</a>
+                </li>
+            </ul>
+        </nav>
+
+        <!-- Content Wrapper. Contains page content -->
+        <div class="content-wrapper">
+            <!-- Content Header (Page header) -->
+            <div class="content-header">
+                <div class="container-fluid">
+                    <div class="row mb-2">
+                        <div class="col-sm-6">
+                            <h1 class="m-0">Daftar Booking Ruangan</h1>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <!-- Main content -->
+            <section class="content">
+                <div class="container-fluid">
+                    <div class="row">
+                        <div class="col-12">
+                            <?php if (!empty($booking)) : ?>
+                                <div class="card">
+                                    <div class="card-body">
+                                        <table class="table table-bordered">
+                                            <thead>
+                                                <tr>
+                                                    <th>No</th>
+                                                    <th>Kode Pinjam</th>
+                                                    <th>Kode Barang</th>
+                                                    <th>Tanggal</th>
+                                                    <th>Waktu Mulai</th>
+                                                    <th>Waktu Selesai</th>
+                                                    <th>Kelas</th>
+                                                    <th>Status</th>
+                                                    <th>Aksi</th>
+                                                </tr>
+                                            </thead>
+                                            <tbody>
+                                                <?php foreach ($booking as $row) : ?>
+                                                    <tr>
+                                                        <td><?= $row['no'] ?></td>
+                                                        <td><?= $row['kode_pinjam'] ?></td>
+                                                        <td><?= $row['kode_ruang'] ?></td>
+                                                        <td><?= $row['tanggal'] ?></td>
+                                                        <td><?= $row['waktu_mulai'] ?></td>
+                                                        <td><?= $row['waktu_selesai'] ?></td>
+                                                        <td><?= $row['kelas'] ?></td>
+                                                        <td><?= $row['status'] ?></td>
+                                                        <td><?= $row['aksi'] ?></td>
+                                                    </tr>
+                                                <?php endforeach; ?>
+                                            </tbody>
+                                        </table>
+                                    </div>
+                                </div>
+                            <?php else : ?>
+                                <p>Tidak ada data booking ruangan.</p>
+                            <?php endif; ?>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </div>
+    </div>
+
+    <!-- AdminLTE JS -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/admin-lte/3.1.0/js/adminlte.min.js"></script>
 </body>
 </html>
