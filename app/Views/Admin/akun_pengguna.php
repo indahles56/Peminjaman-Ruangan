39850c980ae5fc32aedd7f5001681b19b1cb0630
Fajar Sidik Prasetio <fajarsidik1999@gmail.com>
2023-07-08 11:50:33 +0700

    Update akun_pengguna.php

--- app/Views/Admin/akun_pengguna.php
+++ app/Views/Admin/akun_pengguna.php
@@ -1,51 +1,75 @@
 <!DOCTYPE html>
 <html>
-
 <head>
-    <title>Kelola Pengguna</title>
+    <title>Admin Panel - Peminjaman Ruang</title>
+    <link rel="stylesheet" href="<?= base_url('adminlte/dist/css/adminlte.min.css') ?>">
+    <link rel="stylesheet" type="text/css" href="<?= base_url('assets/css/admin.css') ?>">
 </head>
+<body class="hold-transition sidebar-mini">
+    <div class="wrapper">
+        <!-- Navbar -->
+        <nav class="main-header navbar navbar-expand navbar-dark bg-dark">
+            <!-- Navbar content -->
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link" href="<?= base_url('admin/dashboard') ?>">Dashboard</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="<?= base_url('admin/akun_pengguna') ?>">Akun Pengguna</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="<?= base_url('admin/ruang') ?>">Ruang</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="<?= base_url('admin/peminjaman_ruang') ?>">Peminjaman Ruang</a>
+                </li>
+                <!-- Tambahkan menu lainnya jika diperlukan -->
+            </ul>
+        </nav>
 
-<body>
-    <h1>Daftar Pengguna</h1>
+        <!-- Content Wrapper. Contains page content -->
+        <div class="content-wrapper">
+            <!-- Content Header (Page header) -->
+            <div class="content-header">
+                <div class="container-fluid">
+                    <h1 class="m-0">Admin Panel - Peminjaman Ruang</h1>
+                </div>
+            </div>
 
-    <?php
-    // Contoh data pengguna
-    $pengguna = [
-        [
-            'nama_lengkap' => 'John Doe',
-            'email' => 'johndoe@example.com'
-        ],
-        [
-            'nama_lengkap' => 'Jane Smith',
-            'email' => 'janesmith@example.com'
-        ],
-        [
-            'nama_lengkap' => 'Michael Johnson',
-            'email' => 'michaeljohnson@example.com'
-        ]
-    ];
-    ?>
+            <!-- Main content -->
+            <section class="content">
+                <div class="container-fluid">
+                    <h2>Data Peminjaman Ruang</h2>
+                    <!-- Tampilkan data peminjaman ruang sesuai kebutuhan -->
+                    <table class="table">
+                        <thead>
+                            <tr>
+                                <th>ID</th>
+                                <th>Nama Pengguna</th>
+                                <th>Ruang</th>
+                                <th>Tanggal Peminjaman</th>
+                                <th>Durasi (jam)</th>
+                                <!-- Tambahkan kolom lainnya jika diperlukan -->
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <?php foreach ($peminjaman_ruang as $peminjaman) : ?>
+                                <tr>
+                                    <td><?= $peminjaman['id'] ?></td>
+                                    <td><?= $peminjaman['nama_pengguna'] ?></td>
+                                    <td><?= $peminjaman['ruang'] ?></td>
+                                    <td><?= $peminjaman['tanggal_peminjaman'] ?></td>
+                                    <td><?= $peminjaman['durasi'] ?></td>
+                                    <!-- Tambahkan kolom lainnya jika diperlukan -->
+                                </tr>
+                            <?php endforeach; ?>
+                        </tbody>
+                    </table>
+                </div>
+            </section>
+        </div>
+    </div>
 
-    <table>
-        <tr>
-            <th>Nama Lengkap</th>
-            <th>Email</th>
-            <th>Aksi</th>
-        </tr>
-
-        <?php foreach ($pengguna as $data) : ?>
-            <tr>
-                <td><?php echo $data['nama_lengkap']; ?></td>
-                <td><?php echo $data['email']; ?></td>
-                <td>
-                    <!-- Tambahkan aksi sesuai kebutuhan -->
-                    <a href="#">Edit</a>
-                    <a href="#">Hapus</a>
-                </td>
-            </tr>
-        <?php endforeach; ?>
-    </table>
+    <script src="<?= base_url('adminlte/dist/js/adminlte.min.js') ?>"></script>
 </body>
-
 </html>
-
