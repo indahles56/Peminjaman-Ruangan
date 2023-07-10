7b201c142c9342d896218b5782979e490c2d6150
indahles56 <127643042+indahles56@users.noreply.github.com>
2023-07-10 14:02:52 +0700

    ok

--- app/Models/PeminjamanModel.php
+++ app/Models/PeminjamanModel.php
@@ -6,8 +6,8 @@ use CodeIgniter\Model;
 
 class PeminjamanModel extends Model
 {
-    protected $table            = 'peminjaman';
-    protected $primaryKey       = 'kode_pinjam';
+    protected $table = 'peminjaman';
+    protected $primaryKey = 'kode_pinjam';
     protected $useAutoIncrement = true;
-    protected $allowedFields    = ['kode_ruang', 'nama_kegiatan', 'status', 'waktu_pengajuan', 'tanggal', 'waktu_mulai', 'waktu_selesai', 'user', 'admin'];
+    protected $allowedFields = ['kode_ruang', 'user', 'waktu_pengajuan', 'tanggal', 'waktu_mulai', 'waktu_selesai', 'keterangan', 'status', 'admin'];
 }
