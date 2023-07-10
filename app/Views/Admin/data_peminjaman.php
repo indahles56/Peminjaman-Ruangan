e4a011ca350b40e7429235c78553cfb90e7b60ff
meygi123 <127826461+meygi123@users.noreply.github.com>
2023-07-10 16:14:33 +0700

    Update kelola_peminjaman.php

--- app/Views/Admin/data_peminjaman.php
+++ app/Views/Admin/data_peminjaman.php
@@ -1,90 +1,81 @@
-<!-- Tabel Data User [Awal] -->
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Login Ke Peminjaman Ruangan</title>
-  <!-- AdminLTE v3 CSS -->
-  <link rel="stylesheet" href="<?= base_url('adminlte/dist/css/adminlte.min.css') ?>">
-  <!-- AdminLTE v3 JS -->
-  <script src="<?= base_url('adminlte/dist/js/adminlte.min.js') ?>"></script>
-</head>
-
-<body>
-  <table class="table">
-    <thead>
-      <tr>
-        <th>No.</th>
-        <th>Kode Peminjaman</th>
-        <th>Pengguna</th>
-        <th>Ruangan</th>
-        <th>Tanggal</th>
-        <th>Waktu Pengguna</th>
-        <th>Waktu Selesai</th>
-        <th>Keterangan</th>
-        <th>Status Peminjaman</th>
-        <th>Aksi</th>
-      </tr>
-    </thead>
-    <tbody>
-      <?php $no = 1; foreach ($tb as $user): ?>
-      <tr>
-        <td align="center"><?php echo $no; ?></td>
-        <td><?php echo $user['fullname']; ?></td>
-        <td><?php echo $user['username']; ?></td>
-        <td><?php echo $user['level']; ?></td>
-        <td><?php echo $user['tanggal']; ?></td>
-        <td><?php echo $user['waktu_mulai']; ?></td>
-        <td><?php echo $user['waktu_selesai']; ?></td>
-        <td><?php echo $user['keterangan']; ?></td>
-        <td><?php echo $user['status_peminjaman']; ?></td>
-        <td align="center">
-          <div class="btn-group">
-            <a href="" class="btn btn-sm btn-info" data-toggle="modal" data-target="#suntinguser<?php echo $user['username']; ?>" data-backdrop="static">Sunting</a>
-            <a href="" class="btn btn-sm btn-danger" data-toggle="modal" data-target="#hapususer<?php echo $user['username']; ?>" data-backdrop="static">Hapus</a>
-          </div>
-        </td>
-      </tr>
-      <?php $no++; endforeach; ?>
-    </tbody>
-  </table>
-  <!-- Tabel Data User [Akhir] -->
-
-  <!-- Modal Buat User [Awal] -->
-  <div id="buatuser" class="modal fade" role="dialog">
-    <div class="modal-dialog">
-      <!-- Konten Modal [Awal] -->
-      <div class="modal-content">
-        <div class="modal-header">
-          Buat Akun Baru
-          <button type="button" class="close" data-dismiss="modal"><span class="lnr lnr-cross-circle"></span></button>
+<!-- Content Header -->
+<section class="content-header">
+    <div class="container-fluid">
+        <div class="row mb-2">
+            <div class="col-sm-6 h5">
+                Kelola Bookings
+            </div>
         </div>
-
-        <div class="modal-body">
-          <?php echo form_open('dashboard/buat-user'); ?>
-          <ul>
-            <li>
-              <div><i class="lnr lnr-text-format"></i></div><input type="text" name="fullname" placeholder="Nama lengkap" required>
-            </li>
-            <li>
-              <div><i class="lnr lnr-user"></i></div><input type="text" name="username" placeholder="Username" required>
-            </li>
-            <li>
-              <div><i class="lnr lnr-lock"></i></div><input type="password" name="password" placeholder="Password" required>
-            </li>
-            <li>
-              <input type="submit" value="Daftar">
-            </li>
-          </ul>
-          <?php echo form_close(); ?>
-        </div>
-      </div>
-      <!-- Konten Modal [Akhir] -->
     </div>
-  </div>
-  <!-- Modal Buat User [Akhir] -->
-</body>
+</section>
+<!-- /.content-header -->
 
-</html>
+<!-- Main Content -->
+<section class="content">
+    <div class="card">
+        <div class="card-header">
+            <h3 class="card-title">Booking List</h3>
+        </div>
+        <div class="card-body">
+            <table class="table table-bordered">
+                <thead>
+                    <tr>
+                        <th>Kode Pinjam</th>
+                        <th>Kode Ruang</th>
+                        <th>Nama Kegiatan</th>
+                        <th>Status</th>
+                        <th>Waktu Pengajuan</th>
+                        <th>Tanggal</th>
+                        <th>Waktu Mulai</th>
+                        <th>Waktu Selesai</th>
+                        <th>User</th>
+                        <th>Admin</th>
+                        <th>Aksi</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <!-- Loop through the bookings data and generate table rows -->
+                    <?php foreach ($bookings as $booking): ?>
+                        <tr>
+                            <td>
+                                <?= $booking['kode_pinjam'] ?>
+                            </td>
+                            <td>
+                                <?= $booking['kode_ruang'] ?>
+                            </td>
+                            <td>
+                                <?= $booking['nama_kegiatan'] ?>
+                            </td>
+                            <td>
+                                <?= $booking['status'] ?>
+                            </td>
+                            <td>
+                                <?= $booking['waktu_pengajuan'] ?>
+                            </td>
+                            <td>
+                                <?= $booking['tanggal'] ?>
+                            </td>
+                            <td>
+                                <?= $booking['waktu_mulai'] ?>
+                            </td>
+                            <td>
+                                <?= $booking['waktu_selesai'] ?>
+                            </td>
+                            <td>
+                                <?= $booking['user'] ?>
+                            </td>
+                            <td>
+                                <?= $booking['admin'] ?>
+                            </td>
+                            <td>
+                                <button class="btn btn-sm btn-danger">Tolak</button>
+                                <button class="btn btn-sm btn-success">Setujui</button>
+                            </td>
+                        </tr>
+                    <?php endforeach; ?>
+                </tbody>
+            </table>
+        </div>
+    </div>
+</section>
+<!-- /.content -->
