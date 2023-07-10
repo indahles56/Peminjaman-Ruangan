7bed052a40ef4e5b94fd5b465b299bf7469b6a9f
indahles56 <127643042+indahles56@users.noreply.github.com>
2023-07-10 14:02:26 +0700

    ok

--- app/Models/RuanganModel.php
+++ app/Models/RuanganModel.php
@@ -6,37 +6,8 @@ use CodeIgniter\Model;
 
 class RuanganModel extends Model
 {
-    protected $table = 'ruangan';
-    protected$primaryKey = 'kode_ruang';
-    protected $allowedFields = ['nama_ruang', 'kapasitas'];
+    protected $table            = 'ruangan';
+    protected $primaryKey       = 'id';
     protected $useAutoIncrement = true;
-    public function getAllRuangan()
-    {
-        return $this->findAll();
-    }
-    public function tambahRuangan($nama, $kapasitas)
-    {
-        $data = [
-            'nama' => $nama,
-            'kapasitas' => $kapasitas,
-        ];
-
-        return $this->insert($data);
-    }
-
-    public function editRuangan($id, $nama, $kapasitas)
-    {
-        $data = [
-            'nama' => $nama,
-            'kapasitas' => $kapasitas,
-        ];
-
-        return $this->update($id, $data);
-    }
-
-    public function hapusRuangan($id)
-    {
-        return $this->delete($id);
-    }
+    protected $allowedFields    = ['kode_ruang', 'nama_ruang', 'spesifikasi'];
 }
-
