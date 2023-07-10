0a7d683690cc595de29fc58c7fc192cfbfa1040b
meygi123 <127826461+meygi123@users.noreply.github.com>
2023-07-10 16:09:38 +0700

    Update profil.php

--- app/Views/Admin/profil.php
+++ app/Views/Admin/profil.php
@@ -1,86 +1,139 @@
-<!DOCTYPE html>
-<html>
-
-<head>
-    <title>Admin Profile - Peminjaman Ruang Kelas</title>
-    <link rel="stylesheet" href="<?= base_url('adminlte/dist/css/adminlte.min.css') ?>">
-    <!-- AdminLTE v3 JS -->
-    <script src="<?= base_url('adminlte/dist/js/adminlte.min.js') ?>"></script>
-</head>
-
-<body class="hold-transition sidebar-mini">
-    <div class="wrapper">
-        <!-- Navbar -->
-        <nav class="main-header navbar navbar-expand navbar-white navbar-light">
-            <!-- Left navbar links -->
-            <ul class="navbar-nav">
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Beranda</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Tentang</a>
-                </li>
-            </ul>
-            <!-- Right navbar links -->
-            <ul class="navbar-nav ml-auto">
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Logout</a>
-                </li>
-            </ul>
-        </nav>
-
-        <!-- Content Wrapper. Contains page content -->
-        <div class="content-wrapper">
-            <!-- Main content -->
-            <section class="content">
-                <div class="container-fluid">
-                    <h1>Admin Profile</h1>
-
-                    <?php
-                    // User data (retrieve from the database or any other data source)
-                    $user = [
-                        'name' => 'Jane Smith',
-                        'email' => 'janesmith@example.com',
-                        'role' => 'Student',
-                        'classroom' => 'A101',
-                    ];
-                    ?>
-
-                    <h2>Profile Information</h2>
-                    <p><strong>Name:</strong> <?php echo $user['name']; ?></p>
-                    <p><strong>Email:</strong> <?php echo $user['email']; ?></p>
-                    <p><strong>Role:</strong> <?php echo $user['role']; ?></p>
-                    <p><strong>Classroom:</strong> <?php echo $user['classroom']; ?></p>
-
-                    <br>
-
-                    <!-- Room booking form -->
-                    <h2>Room Booking</h2>
-                    <form action="process_booking.php" method="post">
-                        <div class="form-group">
-                            <label for="room">Room:</label>
-                            <input type="text" name="room" id="room" class="form-control" required>
-                        </div>
-
-                        <div class="form-group">
-                            <label for="date">Date:</label>
-                            <input type="date" name="date" id="date" class="form-control" required>
-                        </div>
-
-                        <button type="submit" class="btn btn-primary">Book Room</button>
-                    </form>
-                </div>
-            </section>
-        </div>
-
-        <!-- Footer -->
-        <footer class="main-footer">
-            <div class="float-right d-none d-sm-block">
-                <b>Version</b> 1.0.0
+<!-- Content Header -->
+<section class="content-header">
+    <div class="container-fluid">
+        <div class="row mb-2">
+            <div class="col-sm-6 h5">
+                Profil
             </div>
-        </footer>
+        </div>
     </div>
-</body>
+</section>
+<!-- /.content-header -->
+<?php
 
+?>
+<!-- Main Content -->
+<section class="content">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-6">
+                <div class="card">
+                    <div class="card-header">
+                        <h3 class="card-title">User Information</h3>
+                    </div>
+                    <div class="card-body">
+                        <form id="biodata-form">
+                            <div class="form-group">
+                                <label for="name">Name</label>
+                                <input type="text" class="form-control" id="name" name="name"
+                                    value="<?= $user['name'] ?>" required>
+                            </div>
+                            <div class="form-group">
+                                <label for="email">Email</label>
+                                <input type="email" class="form-control" id="email" name="email"
+                                    value="<?= $user['email'] ?>" required>
+                            </div>
+                            <button type="submit" class="btn btn-primary">Update</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
 
-</html>
+            <div class="col-md-6">
+                <div class="card">
+                    <div class="card-header">
+                        <h3 class="card-title">Change Password</h3>
+                    </div>
+                    <div class="card-body">
+                        <form id="password-form">
+                            <div class="form-group">
+                                <label for="current_password">Current Password</label>
+                                <input type="password" class="form-control" id="current_password"
+                                    name="current_password" required>
+                            </div>
+                            <div class="form-group">
+                                <label for="new_password">New Password</label>
+                                <input type="password" class="form-control" id="new_password" name="new_password"
+                                    required>
+                            </div>
+                            <div class="form-group">
+                                <label for="confirm_password">Confirm New Password</label>
+                                <input type="password" class="form-control" id="confirm_password"
+                                    name="confirm_password" required>
+                            </div>
+                            <button type="submit" class="btn btn-primary" id="change-password-form">Change Password</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- /.content -->
+
+<!-- Edit Password -->
+<script>
+    $(document).ready(function () {
+        $('#biodata-form').on('submit', function (e) {
+            e.preventDefault();
+
+            // Perform client-side validation if needed
+
+            $.ajax({
+                url: 'profile/update',
+                method: 'POST',
+                data: $(this).serialize(),
+                success: function (response) {
+                    if (response.success) {
+                        // Password changed successfully
+                        alert(response.message);
+                        // You can redirect to another page or perform any other action here
+                    } else {
+                        // Error occurred, display error message
+                        alert(response.message);
+                    }
+                },
+                error: function (xhr, status, error) {
+                    // Error occurred, display error message
+                    console.log(error);
+                    alert('An error occurred while changing profile data. Please try again later.');
+                }
+            });
+        });
+    });
+
+</script><!-- Edit Password -->
+<script>
+    $(document).ready(function () {
+        $('#password-form').on('submit', function (e) {
+            e.preventDefault();
+
+            var currentPassword = $('#current-password').val();
+            var newPassword = $('#new-password').val();
+            var confirmPassword = $('#confirm-password').val();
+
+            // Perform client-side validation if needed
+
+            $.ajax({
+                url: 'profile/changepassword',
+                method: 'POST',
+                data: $(this).serialize(),
+                success: function (response) {
+                    if (response.success) {
+                        // Password changed successfully
+                        alert(response.message);
+                        // You can redirect to another page or perform any other action here
+                    } else {
+                        // Error occurred, display error message
+                        alert(response.message);
+                    }
+                },
+                error: function (xhr, status, error) {
+                    // Error occurred, display error message
+                    alert('An error occurred while changing the password. Please try again later.');
+                }
+            });
+        });
+    });
+
+</script>
