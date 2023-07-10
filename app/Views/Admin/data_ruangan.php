d4ceb68347339aef14c63d1904a6e649f10f9434
meygi123 <127826461+meygi123@users.noreply.github.com>
2023-07-10 16:11:47 +0700

    Update kelola_user.php

--- app/Views/Admin/data_ruangan.php
+++ app/Views/Admin/data_ruangan.php
@@ -1,101 +1,226 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Data Ruangan</title>
-    <!-- AdminLTE CSS -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/admin-lte/3.1.0/css/adminlte.min.css">
-</head>
-<body class="hold-transition sidebar-mini">
-    <div class="wrapper">
-        <!-- Navbar -->
-        <nav class="navbar navbar-expand navbar-white navbar-light">
-            <!-- Left navbar links -->
-            <ul class="navbar-nav">
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Home</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">About</a>
-                </li>
-            </ul>
-        </nav>
+<!-- Content Header -->
+<section class="content-header">
+    <div class="container-fluid">
+        <div class="row mb-2">
+            <div class="col-sm-6 h5">
+                Kelola Data Ruangan
+            </div>
+        </div>
+        <button href="" class="btn btn-primary add-room">Tambah Ruangan Baru</button>
+    </div>
+</section>
+<!-- /.content-header -->
 
-        <!-- Content Wrapper. Contains page content -->
-        <div class="content-wrapper">
-            <!-- Content Header (Page header) -->
-            <div class="content-header">
-                <div class="container-fluid">
-                    <div class="row mb-2">
-                        <div class="col-sm-6">
-                            <h1 class="m-0">Data Ruangan</h1>
-                        </div>
+<!-- Main Content -->
+<section class="content">
+    <table id="user-table" class="table table-bordered table-striped">
+        <thead>
+            <tr>
+                <th>Kode Ruang</th>
+                <th>Nama Ruang</th>
+                <th>Spesifikasi</th>
+                <th>Aksi</th>
+            </tr>
+        </thead>
+
+        <tbody>
+            <?php foreach ($rooms as $room): ?>
+                <tr>
+                    <td>
+                        <?= $room['kode_ruang'] ?>
+                    </td>
+                    <td>
+                        <?= $room['nama_ruang'] ?>
+                    </td>
+                    <td>
+                        <?= $room['spesifikasi'] ?>
+                    </td>
+                    <td>
+                        <a href="#" class="btn btn-primary edit-btn" data-id="<?= $room['id'] ?>"
+                            data-json="<?php echo htmlentities(json_encode($room)); ?>">Edit</a>
+                        <a href="" class="btn btn-danger delete-btn" data-id="<?= $room['id'] ?>">Hapus</a>
+                    </td>
+                </tr>
+            <?php endforeach; ?>
+        </tbody>
+
+    </table>
+
+
+</section>
+<!-- /.content -->
+
+
+<!-- Modal -->
+<div class="modal fade" id="addUserModal" tabindex="-1" role="dialog" aria-labelledby="addUserModalLabel"
+    aria-hidden="true">
+    <div class="modal-dialog" role="document" id="myModal">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="addUserModalLabel">Tambah Data Baru</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <form id="form-add-user" class="form">
+                    <div class="form-group">
+                        <label for="name">Kode Ruang</label>
+                        <input type="text" class="form-control" id="kode_ruang" name="kode_ruang" required>
                     </div>
-                </div>
+                    <div class="form-group">
+                        <label for="name">Nama Ruangan</label>
+                        <input type="text" class="form-control" id="nama_ruang" name="nama_ruang" required>
+                    </div>
+                    <div class="form-group">
+                        <label for="name">Spesifikasi</label>
+                        <input type="text" class="form-control" id="spesifikasi" name="spesifikasi" required>
+                    </div>
+
+                    <div class="form-group">
+                        <button type="submit" class="btn btn-primary" id="add-button">Tambah</button>
+                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Batal</button>
+                    </div>
+                </form>
             </div>
 
-            <!-- Main content -->
-            <section class="content">
-                <div class="container-fluid">
-                    <div class="row">
-                        <div class="col-12">
-                            <h2>Daftar Ruangan</h2>
-                            <?php if (!empty($ruangan)) : ?>
-                                <div class="card">
-                                    <div class="card-body">
-                                        <table class="table table-bordered">
-                                            <thead>
-                                                <tr>
-                                                    <th>No</th>
-                                                    <th>Kode Ruang</th>
-                                                    <th>Nama Ruang</th>
-                                                    <th>Aksi</th>
-                                                </tr>
-                                            </thead>
-                                            <tbody>
-                                                <?php foreach ($ruangan as $row) : ?>
-                                                    <tr>
-                                                        <td><?= $row['no'] ?></td>
-                                                        <td><?= $row['kode_barang'] ?></td>
-                                                        <td><?= $row['nama_ruang'] ?></td>
-                                                        <td><?= $row['aksi'] ?></td>
-                                                        <td>
-                                                            <a href="<?= base_url('user/edit-ruangan/' . $row['id']) ?>">Edit</a> |
-                                                            <a href="#">Hapus</a>
-                                                        </td>
-                                                    </tr>
-                                                <?php endforeach; ?>
-                                            </tbody>
-                                        </table>
-                                    </div>
-                                </div>
-                            <?php else : ?>
-                                <p>Tidak ada data ruangan.</p>
-                            <?php endif; ?>
-                            
-                            <h2>Tambah Ruangan</h2>
-                            <div class="card">
-                                <div class="card-body">
-                                    <form action="<?= base_url('user/process-tambah-ruangan') ?>" method="POST">
-                                        <div class="form-group">
-                                            <label for="nama">Nama Ruangan:</label>
-                                            <input type="text" class="form-control" id="nama" name="nama" required>
-                                        </div>
-                                        <div class="form-group">
-                                            <label for="status">Status:</label>
-                                            <input type="text" class="form-control" id="status" name="status" required>
-                                        </div>
-                                        <button type="submit" class="btn btn-primary">Tambah</button>
-                                    </form>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </section>
         </div>
+        <!-- /.modal-content -->
     </div>
+    <!-- /.modal-dialog -->
+</div>
+<!-- /.modal -->
 
-    <!-- AdminLTE JS -->
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/admin-lte/3.1.0/js/adminlte.min.js"></script>
-</body>
-</html>
+
+<!-- Edit Button -->
+<script>
+    $('.delete-btn').on('click', function (e) {
+        e.preventDefault();
+        var id = $(this).data('id');
+        var url = 'admin/rooms/delete/' + id;
+
+        // Send an AJAX request to the delete endpoint
+        $.ajax({
+            url: url,
+            type: 'DELETE',
+            success: function (response) {
+                // Process the response
+                if (response.success) {
+                    // User deleted successfully
+                    alert("Success delete the room");
+
+                    // Update the user list on the page with the updated HTML
+                    $('#content').load('rooms');
+
+                } else {
+                    // Error occurred
+                    alert(response.message);
+                }
+            },
+            error: function (response) {
+                // Error occurred during the AJAX request
+                alert(response.message);
+            }
+        });
+    });
+
+</script>
+
+<!-- Add Button -->
+<script>
+    $('.add-room').on('click', function (e) {
+        e.preventDefault();
+        $('#addUserModal').modal('show');
+
+        $('form').submit(function (e) {
+            e.preventDefault();
+            var data = $(this).serialize();
+            console.log(data);
+            var url = 'add-room';
+            $.ajax({
+                type: "POST",
+                url: url,
+                data: data,
+                success: function (response) {
+                    if (response.success) {
+                        alert('Data Sukses Ditambahkan');
+                        $('#addUserModal').modal('hide');
+                        $('.modal-backdrop').remove();
+                        $('body').removeClass('modal-open');
+                        $('#content').load('rooms');
+
+                    } else {
+                        alert(response.message);
+                        // Jika terdapat error, tampilkan pesan error
+                        $('#addUserModal').modal('hide');
+                        $('.modal-backdrop').remove();
+                        $('body').removeClass('modal-open');
+                        $('#form-add-user .error-message').text(response.message)
+                            .show();
+                    }
+                },
+                error: function (response) {
+                    // Handle error response, display error messages in the modal
+
+                    alert(response.message);
+                }
+            })
+        });
+        $('#addUserModal').modal('hide');
+    })
+</script>
+
+
+<script>
+    $('.edit-btn').on('click', function (e) {
+        e.preventDefault();
+
+        $('#addUserModalLabel').text('Ubah Data Ruang');
+        $('#add-button').text('Konfirmasi Edit');
+
+        var data = $(this).data('json');
+        console.log(data);
+        $('#addUserModal').modal('show');
+
+        $('#kode_ruang').val(data.kode_ruang);
+        $('#nama_ruang').val(data.nama_ruang);
+        $('#spesifikasi').val(data.spesifikasi);
+
+        $('#form-add-user').submit(function (e) {
+            e.preventDefault();
+
+            var url = 'rooms/update/' + data.id;
+            var formData = $(this).serialize();
+            $.ajax({
+                url: url,
+                type: 'POST',
+                data: formData,
+                success: function (response) {
+                    // Process the response
+                    if (response.success) {
+                        // User deleted successfully
+                        alert(response.message);
+                        $('#addUserModal').modal('hide');
+                        $('.modal-backdrop').remove();
+                        $('body').removeClass('modal-open');
+                        $('#content').load('rooms');
+
+                    } else {
+                        console.log(formData);
+
+                        $('#addUserModal').modal('hide');
+                        $('.modal-backdrop').remove();
+                        $('body').removeClass('modal-open');
+                        // Error occurred
+                        alert(response.message);
+                    }
+                },
+                error: function (response) {
+                    // Error occurred during the AJAX request
+                    alert(response.message);
+                }
+            });
+        })
+    })
+
+</script>
