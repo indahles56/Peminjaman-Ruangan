f2ca73ddcdc0add449e868d95327440a979ae7d2
Wiwin WNR <115921167+WiwinWNR@users.noreply.github.com>
2023-07-11 23:09:34 +0700

    ok

--- app/Views/User/dashboard.php
+++ app/Views/User/dashboard.php
@@ -1,121 +1,135 @@
 <!DOCTYPE html>
-<html>
-<head>
-    <title>Dashboard User</title>
-    <!-- AdminLTE CSS -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/admin-lte/3.1.0/css/adminlte.min.css">
-</head>
-<body class="hold-transition sidebar-mini">
-    <div class="wrapper">
-        <!-- Navbar -->
-        <nav class="navbar navbar-expand navbar-white navbar-light">
-            <!-- Left navbar links -->
-            <ul class="navbar-nav">
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Home</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">About</a>
-                </li>
-            </ul>
-        </nav>
+<html lang="en">
 
-        <!-- Content Wrapper. Contains page content -->
-        <div class="content-wrapper">
-            <!-- Content Header (Page header) -->
-            <div class="content-header">
-                <div class="container-fluid">
-                    <div class="row mb-2">
-                        <div class="col-sm-6">
-                            <h1 class="m-0">Dashboard User</h1>
+<head>
+    <?php include('layouts/head.php'); ?>
+</head>
+
+<body background="https://blusignalsystems.com/wp-content/uploads/2016/12/Savin-NY-Website-Background-Web1.jpg">
+    <!-- Content Header -->
+    <?php include('layouts/header.php'); ?>
+    <!-- /.content-header -->
+
+    <!-- Main Content -->
+    <div class="row-cols-2">
+        <div class="relative" style="position: relative; left: 30px; top:120px; width: 50vw;">
+            <h1 style="margin-bottom: 20px;">Haloo</h1>
+            <h3 class="card-header" style="margin-bottom: 20px;">Selamat Datang!!</h3>
+            <h3 class="card-header">Silahkan mulai pengajuan peminjaman ruangan anda!</h3>
+        </div>
+        <div class="row justify-content-center mt-5 relative" style="position: relative; left: 60vw; width: 30vw;">
+            <div class="card text-center col-md-6" style="width: 40rem;">
+                <h5 class="card-header">Pinjam Ruangan</h5>
+                <div class="card-body">
+
+                    <form id="book-form">
+                        <div class="row mb-3">
+                            <label for="nama_lengkap" class="col-sm-4 col-form-label">Nama Lengkap:</label>
+                            <div class="col-sm-8">
+                                <input value="<?= session('user_id') ?>" type="text" id="nama_lengkap" name="user_id"
+                                    class="form-control" hidden>
+                                <input value="<?= session('user_name') ?>" type="text" id="nama_lengkap" name="name"
+                                    class="form-control" required disabled>
+                            </div>
                         </div>
-                    </div>
+                        <div class="row mb-3">
+                            <label for="pilih_ruangan" class="col-sm-4 form-label">Pilih Ruangan:</label>
+                            <div class="col-sm-8">
+                                <select id="pilih_ruangan" name="room_id" class="form-control" required>
+                                    <?php foreach ($rooms as $room) { ?>
+                                        <option value="<?= $room['id'] ?>"><?= $room['kode_ruang'] . ' : ' . $room['nama_ruang'] ?></option>
+                                    <?php } ?>
+                                </select>
+                            </div>
+                        </div>
+
+                        <div class="row mb-3">
+                            <label for="tanggal" class="col-sm-4 form-label">Tanggal:</label>
+                            <div class="col-sm-8">
+                                <input type="date" id="tanggal" name="date" class="form-control" required>
+                            </div>
+                        </div>
+                        <div class="row mb-3">
+                            <label for="jam_mulai" class="col-sm-4 form-label">Jam Mulai:</label>
+                            <div class="col-sm-8">
+                                <input type="time" id="jam_mulai" name="start_time" class="form-control" required>
+                            </div>
+                        </div>
+                        <div class="row mb-3">
+                            <label for="jam_selesai" class="col-sm-4 form-label">Jam Selesai:</label>
+                            <div class="col-sm-8">
+                                <input type="time" id="jam_selesai" name="end_time" class="form-control" required>
+                            </div>
+                        </div>
+                        <div class="row mb-3">
+                            <label for="keterangan" class="col-sm-4 form-label">Keterangan:</label>
+                            <div class="col-sm-8">
+                                <textarea id="keterangan" name="note" class="form-control" rows="3"
+                                    placeholder="Isi dengan tujuan peminjaman, misal rapat." required></textarea>
+                            </div>
+                        </div>
+                        <!-- Add form elements for booking a room here -->
+                        <button type="submit" id="submit-button" class="btn btn-primary">PINJAM SEKARANG</button>
+                    </form>
                 </div>
             </div>
-
-            <!-- Main content -->
+        </div>
+        <div class="wrapper" id="content">
             <section class="content">
                 <div class="container-fluid">
+
                     <div class="row">
                         <div class="col-12">
-                            <h2>Selamat datang di Pinjam Ruang !</h2>
-                            <h2>Pinjam Ruangan Mudah dan Cepat</h2>
-                            <a href="<?= base_url('user/booking') ?>">Buat Booking</a>
                         </div>
                     </div>
                 </div>
             </section>
         </div>
     </div>
+    <!-- /.content -->
 
-    <!-- AdminLTE JS -->
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/admin-lte/3.1.0/js/adminlte.min.js"></script>
+    <!-- build the navbar -->
+    <script>
+        // Get the current page URL
+        var currentUrl = window.location.href;
+
+        // Get the list of navbar links
+        var navbarLinks = document.querySelectorAll('.navbar-nav .nav-link');
+
+        // Loop through each link and check if the URL matches
+        navbarLinks.forEach(function (link) {
+            if (link.href === currentUrl) {
+                link.classList.add('active'); // Add the 'active' class to the matching link
+            }
+        });
+    </script>
+
+    <!-- submit form -->
+    <script>
+        $(document).ready(function () {
+            $('#book-form').submit(function (e) {
+                e.preventDefault();
+                data = $(this).serialize();
+                console.log(data);
+                $.ajax({
+                    url: 'process-booking', // Replace with the actual URL to your PHP controller
+                    type: 'POSt',
+                    data: data,
+                    success: function (response) {
+                        // Handle the response data
+                        console.log(response);
+
+                        alert(response.message);
+                        document.getElementById('book-form').reset();
+                    },
+                    error: function (xhr, status, error) {
+                        // Handle the error if any
+                        console.log(error);
+                    }
+                })
+            });
+        });
+    </script>
 </body>
-</html>
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Booking Disini</title>
-    <!-- AdminLTE CSS -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/admin-lte/3.1.0/css/adminlte.min.css">
-</head>
-<body class="hold-transition sidebar-mini">
-    <div class="wrapper">
-        <!-- Navbar -->
-        <nav class="navbar navbar-expand navbar-white navbar-light">
-            <!-- Left navbar links -->
-            <ul class="navbar-nav">
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Home</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">About</a>
-                </li>
-            </ul>
-        </nav>
 
-        <!-- Content Wrapper. Contains page content -->
-        <div class="content-wrapper">
-            <!-- Content Header (Page header) -->
-            <div class="content-header">
-                <div class="container-fluid">
-                    <div class="row mb-2">
-                        <div class="col-sm-6">
-                            <h1 class="m-0">Booking Disini</h1>
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-            <!-- Main content -->
-            <section class="content">
-                <div class="container-fluid">
-                    <div class="row">
-                        <div class="col-12">
-                            <form action="<?= base_url('user/processBooking') ?>" method="POST">
-                                <label for="nama_lengkap">Nama Lengkap:</label>
-                                <input type="text" id="nama_lengkap" name="nama_lengkap" required>
-                                <label for="pilih_ruangan">Pilih Ruangan:</label>
-                                <input type="text" id="pilih_ruangan" name="pilih_ruangan" required>
-                                <label for="tanggal">Tanggal:</label>
-                                <input type="date" id="tanggal" name="tanggal" required>
-                                <label for="jam_mulai">Jam Mulai:</label>
-                                <input type="date" id="jam_mulai" name="jam_mulai" required>
-                                <label for="jam_selesai">Jam Selesai:</label>
-                                <input type="date" id="jam_selesai" name="jam_selesai" required>
-                                <label for="keterangan">Keterangan:</label>
-                                <input type="text" id="keterangan" name="keterangan" required>
-                                <!-- Tambahkan elemen form untuk booking ruangan di sini -->
-                                <button type="submit">BOOKING</button>
-                            </form>
-                        </div>
-                    </div>
-                </div>
-            </section>
-        </div>
-    </div>
-
-    <!-- AdminLTE JS -->
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/admin-lte/3.1.0/js/adminlte.min.js"></script>
-</body>
 </html>
