5f26f9493167863d56eea9087cc27e848f4dc297
Wiwin WNR <115921167+WiwinWNR@users.noreply.github.com>
2023-07-12 05:11:45 +0700

    Create index.php

---
+++ app/Views/User/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <?php include('includes/head.php'); ?>
+</head>
+
+<body>
+    <!-- <div class="wrapper"> -->
+        <?php include('includes/header.php'); ?>
+
+
+
+        <?php include('includes/footer.php'); ?>
+    <!-- </div> -->
+
+</body>
+
+</html>
