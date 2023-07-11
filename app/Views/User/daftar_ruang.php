4a92f297022e3bd470d8a15a9dfb6bf9a0cb80f7
Wiwin WNR <115921167+WiwinWNR@users.noreply.github.com>
2023-07-11 23:12:26 +0700

    ok

--- app/Views/User/daftar_ruang.php
+++ app/Views/User/daftar_ruang.php
@@ -1,77 +1,64 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
+
 <head>
-    <title>Daftar Ruangan</title>
-    <!-- AdminLTE CSS -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/admin-lte/3.1.0/css/adminlte.min.css">
+    <?php include('layouts/head.php'); ?>
 </head>
-<body class="hold-transition sidebar-mini">
-    <div class="wrapper">
-        <!-- Navbar -->
-        <nav class="navbar navbar-expand navbar-white navbar-light">
-            <!-- Left navbar links -->
-            <ul class="navbar-nav">
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Home</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">About</a>
-                </li>
-            </ul>
-        </nav>
 
-        <!-- Content Wrapper. Contains page content -->
-        <div class="content-wrapper">
-            <!-- Content Header (Page header) -->
-            <div class="content-header">
-                <div class="container-fluid">
-                    <div class="row mb-2">
-                        <div class="col-sm-6">
-                            <h1 class="m-0">Daftar Ruangan</h1>
+<body background="https://blusignalsystems.com/wp-content/uploads/2016/12/Savin-NY-Website-Background-Web1.jpg">
+    <!-- Content Header -->
+    <?php include('layouts/header.php'); ?>
+    <!-- /.content-header -->
+    
+    <!-- Main Content -->
+    <div class="container">
+        <div class="row">
+            <?php $counter = 0; ?>
+            <?php foreach ($rooms as $room): ?>
+                <?php if ($counter % 3 === 0): ?>
+                </div>
+                <div class="row">
+                    <?php endif; ?>
+                    <div class="col-md-4">
+                        <div class="card my-3 mx-3">
+                        <img src="https://cf.bstatic.com/xdata/images/hotel/max1024x768/138495562.jpg?k=7ea8400a0a5ca711d6d3fee2258fcf9920c22701f39d9bafafae0c6dab1f4d6d&o=&hp=1"
+                            class="card-img-top" alt="Room Image">
+                        <div class="card-body">
+                            <h5 class="card-title">
+                                <?= $room['kode_ruang'] ?>
+                            </h5>
+                            <h6 class="card-subtitle mb-2 text-muted">
+                                <?= $room['nama_ruang'] ?>
+                            </h6>
+                            <p class="card-text">
+                                <?= $room['spesifikasi'] ?>
+                            </p>
                         </div>
                     </div>
                 </div>
-            </div>
-
-            <!-- Main content -->
-            <section class="content">
-                <div class="container-fluid">
-                    <div class="row">
-                        <div class="col-12">
-                            <?php if (!empty($ruangan)) : ?>
-                                <div class="card">
-                                    <div class="card-body">
-                                        <table class="table table-bordered">
-                                            <thead>
-                                                <tr>
-                                                    <th>No</th>
-                                                    <th>Kode Barang</th>
-                                                    <th>Nama Barang</th>
-                                                </tr>
-                                            </thead>
-                                            <tbody>
-                                                <?php foreach ($ruangan as $row) : ?>
-                                                    <tr>
-                                                        <td><?= $row['no'] ?></td>
-                                                        <td><?= $row['kode_barang'] ?></td>
-                                                        <td><?= $row['nama_barang'] ?></td>
-                                                    </tr>
-                                                <?php endforeach; ?>
-                                            </tbody>
-                                        </table>
-                                    </div>
-                                </div>
-                            <?php else : ?>
-                                <p>Tidak ada data ruangan.</p>
-                            <?php endif; ?>
-                        </div>
-                    </div>
-                </div>
-            </section>
+                <?php $counter++; ?>
+            <?php endforeach; ?>
         </div>
     </div>
 
-    <!-- AdminLTE JS -->
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/admin-lte/3.1.0/js/adminlte.min.js"></script>
+
+    <!-- /.content -->
+
+
+    <script>
+        // Get the current page URL
+        var currentUrl = window.location.href;
+
+        // Get the list of navbar links
+        var navbarLinks = document.querySelectorAll('.navbar-nav .nav-link');
+
+        // Loop through each link and check if the URL matches
+        navbarLinks.forEach(function (link) {
+            if (link.href === currentUrl) {
+                link.classList.add('active'); // Add the 'active' class to the matching link
+            }
+        });
+    </script>
 </body>
+
 </html>
