b898645a5ba122e3dea608871962cb7c175c85c4
fadilahnuraini <119774568+fadilahnuraini@users.noreply.github.com>
2023-07-12 08:49:52 +0700

    Update RoomsController

--- app/Controllers/User/RoomsController.php
+++ app/Controllers/User/RoomsController.php
@@ -1,1 +1,16 @@
+<?php
 
+namespace App\Controllers\User;
+
+use App\Controllers\BaseController;
+use App\Models\RuanganModel;
+
+class RoomsController extends BaseController
+{
+    public function index()
+    {
+        $roomModel = new RuanganModel();
+        $rooms = $roomModel->findAll();
+        return view('user/daftar_ruangan', ['rooms' => $rooms]);
+    }
+}
