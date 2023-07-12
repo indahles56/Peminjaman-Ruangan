aa6f19eb07e7307d59604b224c21e8f708ca0cce
indahles56 <127643042+indahles56@users.noreply.github.com>
2023-07-12 07:58:44 +0700

    New

---
+++ app/Views/adminlte.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>AdminLTE Example</title>
+    <link rel="stylesheet" href="<?= base_url('css/adminlte.css') ?>">
+</head>
+
+<body class="hold-transition sidebar-mini">
+    <div class="wrapper">
+        <!-- Navbar -->
+        <nav class="navbar navbar-expand navbar-dark bg-dark">
+            <!-- Navbar content -->
+        </nav>
+
+        <!-- Sidebar -->
+        <aside class="main-sidebar sidebar-dark-primary elevation-4">
+            <!-- Sidebar content -->
+        </aside>
+
+        <!-- Content Wrapper -->
+        <div class="content-wrapper">
+            <!-- Main content -->
+            <section class="content">
+                <div class="container-fluid">
+                    <h1>Welcome to AdminLTE</h1>
+                </div>
+            </section>
+        </div>
+
+        <!-- Footer -->
+        <footer class="main-footer">
+            <!-- Footer content -->
+        </footer>
+    </div>
+    <script src="<?= base_url('js/adminlte.js') ?>"></script>
+</body>
+
+</html>
