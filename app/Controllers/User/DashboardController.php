63491b82b519017a9d2b7462dd801e42d1062454
fadilahnuraini <119774568+fadilahnuraini@users.noreply.github.com>
2023-07-12 08:48:25 +0700

    Update DashboardController

--- app/Controllers/User/DashboardController.php
+++ app/Controllers/User/DashboardController.php
@@ -1,1 +1,22 @@
+<?php
 
+namespace App\Controllers\User;
+
+use App\Controllers\BaseController;
+use App\Models\RuanganModel;
+
+class DashboardController extends BaseController
+{
+    public function index()
+    {
+        $request = service('request')->getMethod();
+        $roomModel = new RuanganModel();
+
+        if (!($request === 'post')) {
+            $roomData = $roomModel->findAll();
+            return view('user/dashboard', ['rooms' => $roomData]);
+        }
+
+        return view('user/dashboard', );
+    }
+}
